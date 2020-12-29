dc6dd93c0c701cbe9f4468ea25aee32da77f9e81
Hector Vega <linnk99@gmail.com>
2020-12-28 20:14:00 -0500

    Delete view added with a confirm button that redirects to home

--- blog/blog/views.py
+++ blog/blog/views.py
@@ -1,6 +1,10 @@
 from django.shortcuts import render
 from django.views.generic import ListView, DetailView
-from django.views.generic.edit import CreateView, UpdateView
+from django.views.generic.edit import (
+    CreateView, UpdateView, DeleteView
+    )
+from django.urls import reverse_lazy
+
 from .models import Post
 
 # Create your views here.
@@ -21,3 +25,8 @@ class BlogUpdateView(UpdateView):
     model = Post
     template_name = 'post_edit.html'
     fields = ['title', 'body']
+
+class BlogDeleteView(DeleteView):
+    model = Post
+    template_name = 'post_delete.html'
+    success_url = reverse_lazy('home')
