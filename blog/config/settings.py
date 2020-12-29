2f35eece47f9f6b195affac9c8c4d63ee0c06b90
Hector Vega <linnk99@gmail.com>
2020-12-28 23:45:31 -0500

    Ready to commit to Heroku

--- blog/config/settings.py
+++ blog/config/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'dx1=-d_x9g@6n0mn33s-y-qi^rjmtn&2c@t36u3k0*2f$zl&je'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['.herokuapp.com', 'localhost', '127.0.0.1']
 
 
 # Application definition
