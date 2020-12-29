f8912a22cface73ab1e6832da439aace77c28f6a
Hector Vega <linnk99@gmail.com>
2020-12-28 23:17:43 -0500

    Logout logic and redirect added

--- blog/config/settings.py
+++ blog/config/settings.py
@@ -119,5 +119,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_URL = '/static/'
+
 STATICFILES_DIRS = [str(BASE_DIR.joinpath('static'))]
+
 LOGIN_REDIRECT_URL = 'home'
+LOGOUT_REDIRECT_URL = 'home'
