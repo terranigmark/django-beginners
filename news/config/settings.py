e5a04233b7db771f3fa55771aba91606313981f3
Hector Vega <linnk99@gmail.com>
2021-02-12 21:08:14 -0500

    blog app created and added to installed apps

--- news/config/settings.py
+++ news/config/settings.py
@@ -55,7 +55,7 @@ ROOT_URLCONF = 'config.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [str(BASE_DIR.joinpath('templates'))],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -121,3 +121,6 @@ USE_TZ = True
 STATIC_URL = '/static/'
 
 AUTH_USER_MODEL = 'accounts.CustomUser'
+
+LGOIN_REDIRECT_URL = 'home'
+LOGOUT_REDIRECT_URL = 'home'
