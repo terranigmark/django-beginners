d9039ef588316324dc29cc65c71a0736f6fcdecc
Hector Vega <linnk99@gmail.com>
2021-03-04 23:23:57 -0500

    Sign up view added

--- blog/config/settings.py
+++ blog/config/settings.py
@@ -37,7 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'blog', # new
+    'blog',
+    'accounts', # new
 ]
 
 MIDDLEWARE = [
