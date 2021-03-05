220ffec448fcd849cad5d150e05cf43374c3c9e0
Hector Vega <linnk99@gmail.com>
2021-03-04 21:54:01 -0500

    Update function added and working

--- blog/blog/views.py
+++ blog/blog/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render
 from django.views.generic import ListView, DetailView
-from django.views.generic.edit import CreateView
+from django.views.generic.edit import CreateView, UpdateView
 from .models import Post
 
 # Create your views here.
@@ -16,3 +16,8 @@ class BlogCreateView(CreateView):
     model = Post
     template_name = 'post_new.html'
     fields = ['title', 'author', 'body']
+
+class BlogUpdateView(UpdateView):
+    model = Post
+    template_name = 'post_edit.html'
+    fields = ['title', 'body']
