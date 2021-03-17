dc1ba4e3aca417a512321384833bd53e029dc0ee
Hector Vega <linnk99@gmail.com>
2021-03-16 23:05:08 -0500

    custom user model added

--- news/accounts/forms.py
+++ news/accounts/forms.py
@@ -6,10 +6,10 @@ class CustomUserCreationForm(UserCreationForm):
 
     class Meta(UserCreationForm):
         model = CustomUser
-        fields = UserCreationForm.Meta.fields + ('age',)
+        fields = ('username', 'email', 'age',)
 
 class CustomUserChangeForm(UserChangeForm):
 
     class Meta:
         module = CustomUser
-        fields = UserChangeForm.Meta.fields
+        fields = ('username', 'email', 'age',)
